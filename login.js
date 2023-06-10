283104629ddbaefff30e4ccf08cb72ee7e8eb232
Tommogo-coder <nelsontommogo9@gmail.com>
2023-06-11 00:24:23 +0300

    Auth

--- login.js
+++ login.js
@@ -11,15 +11,16 @@
  
 
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC5KmqA_ZYVKpIWpZpa-r58Epnk0f0B0CU",
-    authDomain: "harvesthq-fd804.firebaseapp.com",
-    projectId: "harvesthq-fd804",
-    storageBucket: "harvesthq-fd804.appspot.com",
-    messagingSenderId: "968917107456",
-    appId: "1:968917107456:web:a24a838d028bbb1343e9e5",
-    measurementId: "G-B3T1B5TZ9N"
-  };
+  apiKey: "AIzaSyC5KmqA_ZYVKpIWpZpa-r58Epnk0f0B0CU",
+  authDomain: "harvesthq-fd804.firebaseapp.com",
+  projectId: "harvesthq-fd804",
+  storageBucket: "harvesthq-fd804.appspot.com",
+  messagingSenderId: "968917107456",
+  appId: "1:968917107456:web:a24a838d028bbb1343e9e5",
+  measurementId: "G-B3T1B5TZ9N"
+};
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
