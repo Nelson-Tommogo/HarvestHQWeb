fd2c92ee28809f531f7c13ee070da1dc8e9fc72c
Tommogo-coder <nelsontommogo9@gmail.com>
2023-06-10 20:43:28 +0300

    Auth

--- login.js
+++ login.js
@@ -23,31 +23,29 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   const database= getDatabase(app);
   const auth = getAuth();
-
-  SignUp.addEventListener('click',(e) => {
-var email = document.getElementById('email').value;
-var password = document.getElementById('password').value;
-var username = document.getElementById('username').value;
-
-createUserWithEmailAndPassword(auth, email, password)
-.then((userCredential) => {
- // Signed in 
-  const user = userCredential.user;
-
-  set(ref(database, 'users/' + user.uid),{
-      username: username,
-      email: email
-  })
-
-  alert('Account Created Successfully!');
-  window.location.href = 'login.html';
-  // ...
-})
-.catch((error) => {
-  const errorCode = error.code;
-  const errorMessage = error.message;
-
-  alert(errorMessage);
-// ..
-});
+  login.addEventListener('click',(e)=>{
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+ 
+       signInWithEmailAndPassword(auth, email, password)
+       .then((userCredential) => {
+         // Signed in 
+         const user = userCredential.user;
+ 
+         const dt = new Date();
+          update(ref(database, 'users/' + user.uid),{
+           last_login: dt,
+         })
+ 
+          alert('User loged in!');
+          window.location.href = 'index.html';
+         // ...
+       })
+       .catch((error) => {
+         const errorCode = error.code;
+         const errorMessage = error.message;
+ 
+         alert(errorMessage);
+   });
+ 
 })
